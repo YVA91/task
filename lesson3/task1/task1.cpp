b86f5069e6f5f0b6990e0e7dbcbd22ab65cf2df8
Vladimir Iakhnev <Yahnev91@yandex.ru>
2023-08-01 21:46:58 +0300

    Лекция 3

---
+++ lesson3/task1/task1.cpp
@@ -0,0 +1,116 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+class Calculator {
+    public:
+        double num1;
+        double num2;
+    
+        Calculator() { }
+
+    double add(double num1, double num2) {
+        return num1 + num2;
+     };
+
+    double multiply(double num1, double num2) {
+        return num1 * num2;
+    };
+
+    double subtract_1_2(double num1, double num2) {
+        return num1 - num2;
+    };
+
+    double subtract_2_1(double num1, double num2) {
+        return num2 - num1;
+    };
+
+    double divide_1_2(double num1, double num2) {
+        return num1/num2;
+    };
+
+    double divide_2_1(double num1, double num2) {
+        return num2 / num1;
+    };
+
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        };
+
+    };
+
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        };
+    };
+};
+
+
+double checkNum1(Calculator calculator) {
+    double num1;
+    std::cout << "Введите num1: ";
+    std::cin >> num1;
+
+    if (!calculator.set_num1(num1)) {
+        std::cout << "Неверный ввод!" << std::endl;
+        checkNum1(calculator);
+    }
+    else {
+        return num1;
+    }
+    
+}
+
+double checkNum2(Calculator calculator) {
+    double num2;
+    std::cout << "Введите num2: ";
+    std::cin >> num2;
+
+    if (!calculator.set_num2(num2)) {
+        std::cout << "Неверный ввод!" << std::endl;
+        checkNum2(calculator);
+    }
+    else {
+        return num2;
+    }
+
+}
+
+
+
+void useCalculator() {
+
+    Calculator calculator;
+
+    double num1 = checkNum1(calculator);
+    double num2 = checkNum2(calculator);
+    
+    std::cout << "num1 + num2 = " << calculator.add(num1, num2) << std::endl;
+    std::cout << "num1 - num2 = " << calculator.subtract_1_2(num1, num2) << std::endl;
+    std::cout << "num2 - num1 = " << calculator.subtract_2_1(num1, num2) << std::endl;
+    std::cout << "num1 * num2 = " << calculator.multiply(num1, num2) << std::endl;
+    std::cout << "num1 / num2 = " << calculator.divide_1_2(num1, num2) << std::endl;
+    std::cout << "num2 / num1 = " << calculator.divide_2_1(num1, num2) << std::endl;
+   
+    useCalculator();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    useCalculator();
+}
+
+
---
+++ lesson3/task2/task2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <locale>
+#include <Windows.h>
+
+
+class Counter {
+private:
+    int count;
+public:
+    Counter() {
+        this->count = 1;
+    }
+    Counter(int count) {
+        this->count = count;
+    }
+
+    void сountPlus() {
+        this->count++;
+    }
+
+    void сountMinus() {
+        this->count--;
+    }
+
+    void printCount() {
+        std::cout << this->count << std::endl;
+    }
+
+};
+
+void consolWork(Counter counter) {
+    std::string x;
+    std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+    std::cin >> x;
+
+    if (x == "x") {
+        std::cout << "До свидания!";
+        return;
+    }
+    else if (x == "+") {
+        counter.сountPlus();
+        consolWork(counter);
+    }
+    else if (x == "-") {
+        counter.сountMinus();
+        consolWork(counter);
+    }
+    else if (x == "=") {
+        counter.printCount();
+        consolWork(counter);
+    }
+    else {
+        std::cout << "Введена не верная команда!" << std::endl;
+        consolWork(counter);
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    std::string ask;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> ask;
+    std::transform(ask.begin(), ask.end(), ask.begin(), ::tolower);
+    std::cout << std::endl;
+
+    Counter counter;
+    if (ask == "да") {
+        int x;
+        std::cout << " Введите начальное значение счётчика: ";
+        std::cin >> x;
+        counter = Counter(x);
+    }
+
+    consolWork(counter);
+
+}
