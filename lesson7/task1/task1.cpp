da63adb1a3003e5f7e44d5e0d50b373b6ef78d6f
Vladimir Iakhnev <Yahnev91@yandex.ru>
2023-08-26 14:25:43 +0300

    Лекция 7

---
+++ lesson7/task1/task1.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+
+#define MODE 3
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+#if defined(MODE) 
+    #if MODE == 0 
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+    #elif MODE == 1 
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int x, y;
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << std::endl;
+    std::cout << "Введите число 2: ";
+    std::cin >> y;
+    std::cout << std::endl;
+    std::cout << "Результат сложения: " << add(x, y) << std::endl;
+    #else 
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+    #endif
+#else 
+    std::cout << "Необходимо определить MODE" << std::endl;
+#endif
+
+}
---
+++ lesson7/task2/task2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+#define SUB(a,b) ((a) - (b))
+
+int main()
+{
+    int a = 6, b = 5, c = 2;
+
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+
+}
